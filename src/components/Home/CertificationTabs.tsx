3cd70ddbb281d814dd2b5162aa6367e05da29f4d
Sudheer Kumar <j.sokoli@elogic.com.ua>
2026-02-10 10:04:22 +0530

    tab

--- src/components/Home/CertificationTabs.tsx
+++ src/components/Home/CertificationTabs.tsx
@@ -146,7 +146,7 @@ export default function CertificationTabs() {
         </div> 
 
         {/* Cards */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 mt-12">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 mt-8">
           {tabCourses[activeTab].map((course) => (
             <CourseCard key={course.id} course={course} />
           ))}

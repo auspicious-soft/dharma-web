76347d574047ac488e03ed918e50c4cbab960b17
Anil <info@auspicioussoft.com>
2026-02-10 16:11:47 +0530

    footer

--- src/components/Footer/Newsletter.tsx
+++ src/components/Footer/Newsletter.tsx
@@ -4,11 +4,11 @@ import { Button } from '../ui/button'
 export default function Newsletter() {
     return (
         <div className='footer-newsletter'>
-            <div className="self-stretch p-10 bg-white/5 rounded-[20px] flex justify-between items-center">
+            <div className="self-stretch p-10 bg-white/5 rounded-[20px] flex justify-between items-center md:flexr">
                 <p className="max-w-[541px] justify-start text-white text-base font-normal leading-8">Subscribe to our newsletter to received product update and promotions</p>
-                <div className="flex justify-start items-center gap-2.5 max-w-[394px] w-full">
-                    <input type="text" placeholder='Email Address...' className='w-full h-11 max-w-64 text-light-blue py-3 px-4 bg-transparent rounded-[30px] outline outline-1 outline-offset-[-1px] outline-[#F0F8FF80] text-xs font-normal leading-6' />
-                    <Button>Submit</Button>
+                <div className="grid grid-cols-[1fr_131px] justify-start items-center gap-2.5 max-w-[394px] w-full">
+                    <input type="text" placeholder='Email Address...' className='w-full h-11 text-light-blue py-3 px-4 bg-transparent rounded-[30px] outline outline-1 outline-offset-[-1px] outline-[#F0F8FF80] text-xs font-normal leading-6' />
+                    <Button className='bg-[#4C8DEA]'>Submit</Button>
                 </div>
             </div>
         </div>
